747129e7ffe6026a14fc3fbb6c50305b615fbac7
imKashyap <rahulkashyap4041@gmail.com>
2020-03-30 18:20:56 +0530

    Update Homepage

--- lib/screens/homeScreen/home_screen.dart
+++ lib/screens/homeScreen/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:zerodollarsecurity/screens/homeScreen/widgets/backend.dart';
+import 'package:zerodollarsecurity/screens/homeScreen/widgets/card_view.dart';
+import 'package:zerodollarsecurity/screens/homeScreen/widgets/detailView.dart';
 import 'package:zerodollarsecurity/screens/homeScreen/widgets/frontend.dart';
 import 'package:zerodollarsecurity/screens/homeScreen/widgets/side_drawer.dart';
+import 'package:zerodollarsecurity/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   static String id = 'HomeScreen';
@@ -27,17 +30,74 @@ class _HomeScreenState extends State<HomeScreen> {
 class TopTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Backend(),
-        ListView(
-          children: <Widget>[
-            Frontend(),
-          ],
-        ),
-      ],
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Backend(),
+              SizedBox(
+                height: 40.0,
+              ),
+              Expanded(
+                child: Center(
+                  child: CardView(),
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: <Widget>[
+              ListView(
+                children: <Widget>[
+                  Frontend(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CircleAvatar(
+                    child: CircleAvatar(
+                      radius: 75.0,
+                      backgroundColor: Colors.black,
+                      backgroundImage: AssetImage('assets/profile.jpeg'),
+                    ),
+                    radius: 80.0,
+                    backgroundColor: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  DetailView(),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      print('working');
+                    },
+                    child: Container(
+                      height: 80.0,
+                      color: Colors.blue[900],
+                      child: Center(
+                        child: Text(
+                          'Tap for QR',
+                          style: kTextStyle.copyWith(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
-
